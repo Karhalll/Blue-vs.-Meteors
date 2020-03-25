b4b0bab58e44be39eb43449684a5a9c9c20db948
Karhal <mar.fucik@gmail.com>
2020-03-25 22:21:58 +0100

    Change Player size. Resolution 1280x720. On Death Screen.

---
+++ Assets/Scripts/Fader.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fader : MonoBehaviour
+{
+    CanvasGroup canvasGroup;
+    Coroutine currentActiveFade = null;
+
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void FadeOutImmediate()
+    {
+        canvasGroup.alpha = 1;
+    }
+
+    public void FadeOut(float time)
+    {
+        Fade(1, time);
+    }
+
+    public void FadeIn(float time)
+    {
+        Fade(0, time);
+    }
+
+    private void Fade(float target, float time)
+    {
+        if (currentActiveFade != null)
+        {
+            StopCoroutine(currentActiveFade);
+        }
+        currentActiveFade = StartCoroutine(FadeRoutine(target, time));
+    }
+
+    private IEnumerator FadeRoutine(float target, float time)
+    {
+        while (!Mathf.Approximately(canvasGroup.alpha, target))
+        {
+            canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, target, Time.deltaTime / time);
+            yield return null;
+        }
+    }
+}
+
---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerDeath : MonoBehaviour
+{
+    [SerializeField] bool isInvincible = false;
+    [SerializeField] UnityEvent deathEvent = null;
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.tag == "Meteor" && !isInvincible)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        deathEvent.Invoke();
+    }
+}
